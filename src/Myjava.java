9fcc4b0dcbec73b1629e1b6b5c119c17a17e9280
Abhinav Kasaudhan <abhinavka@PROD.HCLPNP.COM>
2019-01-02 12:38:34 +0530

    changes 3

--- src/Myjava.java
+++ src/Myjava.java
@@ -4,6 +4,6 @@ public class Myjava {
     public static void main(String[] args) {
         System.out.println("Hi everyone.... For New Brnach");
         System.out.println("hiiii");
-        System.out.println("hooooo");
+        System.out.println("hooooo"+"hi");
     }
 }
